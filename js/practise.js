090b8c1c73417d66971601877a6c54ee768f5b78
Muradovvv <muradovv.murad2706@gmail.com>
2024-10-30 01:11:20 +0400

    file add

---
+++ js/practise.js
@@ -0,0 +1,8 @@
+let age = 20;
+if(age < 18){
+    console.log(`${age} yeniyetme`);
+} else if(age >18 && age < 64){
+    console.log(`${age} yetkin`);
+} else if(age > 64){
+    console.log(`${age} yasli`);
+}
